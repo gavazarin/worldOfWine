e870754fdb2150b54ff837c277a0e3dd4b9e79f4
gavazarin <gavazarin@gmail.com>
2020-11-16 11:50:14 -0300

    Bilhete Gift

--- js/bilhetes.js
+++ js/bilhetes.js
@@ -10,10 +10,10 @@ const open3exp = document.querySelector('.btn-3exp');
 open3exp.addEventListener('click', function() {
     mostrarBilhetes('cards-3exp');
 })
-const open6exp = document.querySelector('.btn-6exp');
-open6exp.addEventListener('click', function() {
-    mostrarBilhetes('cards-6exp');
-})
+// const open6exp = document.querySelector('.btn-6exp');
+// open6exp.addEventListener('click', function() {
+//     mostrarBilhetes('cards-6exp');
+// })
 const openPrata = document.querySelector('.btn-prata');
 openPrata.addEventListener('click', function() {
     mostrarBilhetes('cards-prata');
@@ -26,10 +26,10 @@ const openCork = document.querySelector('.btn-cork');
 openCork.addEventListener('click', function() {
     mostrarBilhetes('cards-cork');
 })
-const openFashion = document.querySelector('.btn-fashion');
-openFashion.addEventListener('click', function() {
-    mostrarBilhetes('cards-fashion');
-})
+// const openFashion = document.querySelector('.btn-fashion');
+// openFashion.addEventListener('click', function() {
+//     mostrarBilhetes('cards-fashion');
+// })
 const openVessels = document.querySelector('.btn-vessels');
 openVessels.addEventListener('click', function() {
     mostrarBilhetes('cards-vessels');
@@ -52,10 +52,10 @@ const close3exp = document.querySelector('.close-3exp');
 close3exp.addEventListener('click', function() {
     fecharBilhetes('cards-3exp');
 })
-const close6exp = document.querySelector('.close-6exp');
-close6exp.addEventListener('click', function() {
-    fecharBilhetes('cards-6exp');
-})
+// const close6exp = document.querySelector('.close-6exp');
+// close6exp.addEventListener('click', function() {
+//     fecharBilhetes('cards-6exp');
+// })
 const closePrata = document.querySelector('.close-prata');
 closePrata.addEventListener('click', function() {
     fecharBilhetes('cards-prata');
@@ -68,10 +68,10 @@ const closeCork = document.querySelector('.close-cork');
 closeCork.addEventListener('click', function() {
     fecharBilhetes('cards-cork');
 })
-const closeFashion = document.querySelector('.close-fashion');
-closeFashion.addEventListener('click', function() {
-    fecharBilhetes('cards-fashion');
-})
+// const closeFashion = document.querySelector('.close-fashion');
+// closeFashion.addEventListener('click', function() {
+//     fecharBilhetes('cards-fashion');
+// })
 const closeVessels = document.querySelector('.close-vessels');
 closeVessels.addEventListener('click', function() {
     fecharBilhetes('cards-vessels');
