95881bffbcd047d2ecaf0deee3136b0374574c43
Gabriel <gavazarin@gmail.com>
2021-04-28 15:35:28 -0300

    workshop + tabs + inicio swiper

---
+++ includes/workshop.js
@@ -0,0 +1,42 @@
+document.querySelectorAll('.dropdown-menu').forEach(function (element) {
+    element.addEventListener('click', function (e) {
+        e.stopPropagation();
+    });
+})
+
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 7,
+    spaceBetween: 0,
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 0
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 3,
+            spaceBetween: 0
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 4,
+            spaceBetween: 0
+        },
+        // when window width is >= 992px
+        992: {
+            slidesPerView: 7,
+            spaceBetween: 0
+        }
+    }
+});
