1e63810ab0e7abd0d79ddecb7f109cbbc14c883a
Gabriel <gavazarin@gmail.com>
2021-05-10 16:13:20 -0300

    cards carousel / desktop + responsivo

--- includes/cards-swiper.js
+++ includes/cards-swiper.js
@@ -1,26 +1,47 @@
 const swiper = new Swiper('.cards-swiper', {
-    // Default parameters
-    slidesPerView: 3,
-    spaceBetween: 0,
-    direction: 'horizontal',
-    loop: false,
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+  direction: 'horizontal',
+  loop: false,
 
-    // Responsive breakpoints
-    breakpoints: {
-      // when window width is >= 320px
-      320: {
-        slidesPerView: 2,
-        spaceBetween: 20
-      },
-      // when window width is >= 480px
-      480: {
-        slidesPerView: 3,
-        spaceBetween: 30
-      },
-      // when window width is >= 640px
-      640: {
-        slidesPerView: 3,
-        spaceBetween: 15
-      }
+    // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+
+  // Responsive breakpoints
+  breakpoints: {
+    375: {
+      slidesPerView: 1.1,
+      spaceBetween: 15
+    },
+    // when window width is >= 580px
+    580: {
+      slidesPerView: 1.2,
+      spaceBetween: 30
+    },
+    // when window width is >= 850px
+    850: {
+      slidesPerView: 2.1,
+      spaceBetween: 30
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 2.3,
+      spaceBetween: 30
+    },
+    // when window width is >= 1355px
+    1355: {
+      slidesPerView: 3.2,
+      spaceBetween: 30
     }
+  }
 })
